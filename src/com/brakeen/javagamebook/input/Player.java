bc0a2852f5bc51b0fa68d5184c496572381b48ea
curtesmith <curt.e.smith@gmail.com>
2013-04-18 20:11:04 -0400

    added Player class

---
+++ src/com/brakeen/javagamebook/input/Player.java
@@ -0,0 +1,66 @@
+package com.brakeen.javagamebook.input;
+
+
+import com.brakeen.javagamebook.graphics.*;
+
+public class Player extends Sprite
+{
+    public static final int STATE_NORMAL = 0;
+    public static final int STATE_JUMPING = 1;
+    public static final float SPEED = .3f;
+    public static final float GRAVITY = .002f;
+    private int floorY;
+    private int state;
+    
+    
+    public Player(Animation anim)
+    {
+        super(anim);
+        state = STATE_NORMAL;
+    }
+    
+    
+    public int getState()
+    {
+        return state;
+    }
+    
+    
+    public void setState(int state)
+    {
+        this.state = state;
+    }
+    
+    
+    public void setFloor(int floorY)
+    {
+        this.floorY = floorY;
+        setY(floorY);
+    }
+    
+    
+    public void jump()
+    {
+        setVelocityY(-1);
+        state = STATE_JUMPING;
+    }
+    
+    
+    public void update(long elapsedTime)
+    {
+        if(getState() == STATE_JUMPING)
+        {
+            setVelocityY(getVelocityY() + GRAVITY * elapsedTime);
+        }
+        
+        super.update(elapsedTime);
+        
+        if(getState() == STATE_JUMPING && getY() >= floorY)
+        {
+            setVelocityY(0);
+            setY(floorY);
+            setState(STATE_NORMAL);
+            
+        }
+    }
+}
